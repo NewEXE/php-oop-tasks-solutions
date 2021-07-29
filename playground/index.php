3ef5bb0bf432ae7db40285183bc309ed0a31b25a
NewEXE <v.voloshyn96@gmail.com>
2021-07-29 04:05:26 +0300

    Add playground example

--- playground/index.php
+++ playground/index.php
@@ -1,15 +1,33 @@
+<pre>
 <?php
 /*
  * For code testing
  */
 
-$arr = [1, 2, 3];
+class User
+{
+    public string $name;
 
-removeFirstItem($arr);
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+}
+
+$user1 = new User('Иван');
+$user2 = new User('Пётр');
+$user3 = new User('Инокентий');
+
+$arr = [$user1, $user2, $user3];
+
+setNameToFirstItem($arr, 'Степан');
 
 var_dump($arr);
 
-function removeFirstItem($arr)
+function setNameToFirstItem(array $arr, string $name)
 {
-    unset($arr[0]);
+    $arr[0]->name = $name;
 }
+
+?>
+</pre>
